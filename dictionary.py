b2a767a5c1df8647e4c2beed8089d5c24b5524b8
obonyodorice <obonyodorice3@gmail.com>
2025-02-24 10:21:12 +0300

    cn

--- dictionary.py
+++ dictionary.py
@@ -5,10 +5,33 @@ def Sibling_intro(Dictionary):
     while True:
         Sibling_name =input('My name is:')
         Sibling_colour=input('My favourite colour is is:')
-        Sibling_colours[Sibling_name]=Sibling_colour    
+        Sibling_colours[Sibling_name]=Sibling_colour   
+
 another=input('add another(y/n)')
 if another=='y':
-  continue
+    continue
 else:
     break
 
+# name = input('Enter your name: ')
+# color = input('Enter your favorite color:')
+# name1 = input('Enter your name: ')
+# color1 = input('Enter your favorite color:')
+# name2 = input('Enter your name: ')
+# color2 = input('Enter your favorite color:')
+# name3 = input('Enter your name: ')
+# color3 = input('Enter your favorite color:')
+
+
+# family = {
+#     name: color,
+#     name1: color1,
+#     name2: color2,
+#     name3: color3
+# }
+
+# def Sibling_intro(Dictionary):
+#     for key,val in Dictionary.items():
+#         print(f'I am {key} and {val} is my favourite colour')
+
+# Sibling_intro(family)
---
+++ family.py
@@ -0,0 +1,26 @@
+def family():
+    favourite={}
+
+    while True:
+
+        
+        also=input('Do you wanna continue (y/n)')
+
+        if also=='n':
+           break
+        
+        elif also == 'y':
+            name=input('Enter your name:')
+            colour=input('Whats your favourite colour:')
+            favourite[name] = colour
+        # else:
+        #    continue
+
+
+        
+    
+    for key, value in favourite.items():
+         print(f'{key}:{value}')
+
+family()
+
