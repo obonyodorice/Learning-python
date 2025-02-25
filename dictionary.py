568b18ef617875c6e456b2fb54bbc7dba9d9de3d
obonyodorice <obonyodorice3@gmail.com>
2025-02-25 12:54:38 +0300

    Planets

--- dictionary.py
+++ dictionary.py
@@ -1,15 +1,15 @@
-def Sibling_intro(Dictionary):
-    for key,val in Dictionary.items():
-        print(f'I am {key} and {val} is my favourite colour')
-        Sibling_colours={}
-    while True:
-        Sibling_name =input('My name is:')
-        Sibling_colour=input('My favourite colour is is:')
-        Sibling_colours[Sibling_name]=Sibling_colour   
+# def Sibling_intro(Dictionary):
+#     for key,val in Dictionary.items():
+#         print(f'I am {key} and {val} is my favourite colour')
+#         Sibling_colours={}
+#     while True:
+#         Sibling_name =input('My name is:')
+#         Sibling_colour=input('My favourite colour is is:')
+#         Sibling_colours[Sibling_name]=Sibling_colour   
 
-    another=input('add another(y/n)')
-if another=='n':
-     break
+#     another=input('add another(y/n)')
+# if another=='n':
+#      break
 
 
    
--- functions.py
+++ functions.py
@@ -2,17 +2,25 @@
 #     print(f'Good {time} {name} Hope you are fine')
 # greet('Dorice','Morning')
 class Planet:
-    def __init__(self):
-        self.name='Earth'
-        self.radius=3600
-        self.gravity=9.8
-        self.system='Earth system'
+    def __init__(self,name,radius,gravity,system):
+        self.name= name
+        self.radius= radius
+        self.gravity= gravity
+        self.system= system
     def orbit(self):
         return f'{self.name} is orbiting in the {self.system}'
 
-Earth= Planet()
+Earth= Planet( 'Earth',30000,9.8,'Earth system')
 print(f'Name is:{Earth.name}')  
 print(f'radius is:{Earth.radius}') 
 print(f'gravity is:{Earth.gravity}') 
 print(f'system is:{Earth.system}') 
 print(Earth.orbit())
+
+Venus= Planet('Venus',50000,0.9,'Venus system')
+print(f'Name is:{Venus.name}')  
+print(f'radius is:{Venus.radius}') 
+print(f'gravity is:{Venus.gravity}') 
+print(f'system is:{Venus.system}') 
+print(Venus.orbit())
+
