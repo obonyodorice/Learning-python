a73d62e246c2404725ce14a3dcc3a066dd88579c
obonyodorice <obonyodorice3@gmail.com>
2025-02-26 20:38:27 +0300

    ifelse

---
+++ ifelse.py
@@ -0,0 +1,11 @@
+ 
+a = int(input('Enter a number:'))
+
+def num(a): 
+    if a%2 == 0:
+        print('The number is even')
+    else:
+        print('The number is odd')
+
+num(a)
+    
--- patient.py
+++ patient.py
@@ -1,9 +1,9 @@
-name=input('Whats your name:')
-age=input('Whats your age:')
-def patient(name,age):
+# name=input('Whats your name:')
+# age=input('Whats your age:')
+# def patient(name,age):
     
-    print(f'Patients name is: {name}')
-    print(f'He is {age} year old')
-    print('He is a new patient')
+#     print(f'Patients name is: {name}')
+#     print(f'He is {age} year old')
+#     print('He is a new patient')
     
-patient(name,age)
+# patient(name,age)
