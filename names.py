0e1917f0d9843b3f38a32e48505c9fb35a781add
obonyodorice <obonyodorice3@gmail.com>
2025-03-03 16:05:17 +0300

    list

--- names.py
+++ names.py
@@ -1,7 +1,9 @@
 
-names=('Dorice','Bright','Val','Mervel','Reagan')
-for n in range(len(names)):
-    print (f'{n}.{names[n]}')
+# names=('Dorice','Bright','Val','Mervel','Reagan')
+# for n in range(len(names)):
+#     print (f'{n}.{names[n]}')
 
-
+colours =['yellow','green','purple','indigo','violet']
+for i,colour in enumerate(colours):
+    print(f'{i}.{colours[i]}')
 
