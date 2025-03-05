034951defb27881f923fd00f9d1cc98e6976b72a
obonyodorice <obonyodorice3@gmail.com>
2025-03-05 11:19:19 +0300

    grades

--- functions.py
+++ functions.py
@@ -1,31 +1,19 @@
-# def greet(name,time):
 #     print(f'Good {time} {name} Hope you are fine')
 # greet('Dorice','Morning')
 
-# class Planet:
-#     shape ='Round'
-#     def __init__(self,name,radius,gravity,system):
-#         self.name= name
-#         self.radius= radius
-#         self.gravity= gravity
-#         self.system= system
-#     def orbit(self):
-#         return f'{self.name} is orbiting in the {self.system}'
-    
-# Earth= Planet( 'Earth',30000,9.8,'Earth system')
-# print(f'Name is:{Earth.name}')  
-# print(f'radius is:{Earth.radius}') 
-# print(f'gravity is:{Earth.gravity}') 
-# print(f'system is:{Earth.system}') 
-# print(Earth.orbit())
+class Students:
+    def __init__(Self,Name,Stream,Adm,Position):
+        Self.Name=Name
+        Self.Stream=Stream
+        Self.Adm=Adm
+        Self.Position=Position
 
-# Venus= Planet('Venus',50000,0.9,'Venus system')
-# print(f'Name is:{Venus.name}')  
-# print(f'radius is:{Venus.radius}') 
-# print(f'gravity is:{Venus.gravity}') 
-# print(f'system is:{Venus.system}') 
-# print(Venus.orbit())
-x = 'Pretty'
-def my_desc():
-    print(f'Dorice is always',x)
-my_desc()
+    def remarks(Self):
+       return f'{Self.Name}is proceeding to the next class'
+
+Pass= Students('Dorice','Mars 3','13699','1')
+print(f'Name is:{Pass.Name}') 
+print(f'Stream:{Pass.Stream}')   
+print(f'Admission no.:{Pass.Adm}')
+print(f'Overral position:{Pass.Position}')
+print(Pass.remarks())
