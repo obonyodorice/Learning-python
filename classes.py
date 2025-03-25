448c1bfad264f8668396d78f580c67626e296d47
obonyodorice <obonyodorice3@gmail.com>
2025-03-25 18:14:34 +0300

    Mod

--- classes.py
+++ classes.py
@@ -1,10 +1,17 @@
-class Parent:
-    hair_colour = 'Black'
+# class Parent:
+#     hair_colour = 'Black'
 
-class Child(Parent):
+# class Child(Parent):
+#     pass
+
+# dorice = Child()
+# mom_dorice = Parent()
+
+# print(f'The hair colour of mom_dorice is:{mom_dorice.hair_colour} and that of dorice is:{dorice.hair_colour}')
+class student:
+    best_colour='Pink'
+class teacher(student):
     pass
-
-dorice = Child()
-mom_dorice = Parent()
-
-print(f'The hair colour of mom_dorice is:{mom_dorice.hair_colour} and that of dorice is:{dorice.hair_colour}')
+Dorice= teacher()
+Child_Dorice=student()
+print(f'{Dorice.best_colour} is my favourite colour and that of my child is also {Child_Dorice.best_colour}')
