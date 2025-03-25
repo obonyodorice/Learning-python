47b1c8cead48686fa610795fbbb723a7cf8486b5
obonyodorice <obonyodorice3@gmail.com>
2025-03-25 18:04:20 +0300

    Com

--- dorris.py
+++ dorris.py
@@ -8,17 +8,33 @@
 # trapezium_area(4, 5)
 
 
-class Trapezia:
+# class Trapezia:
 
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
+#     def __init__(self, a, b):
+#         self.a = a
+#         self.b = b
 
-    def area(self, height):
+#     def area(self, height):
         
-        area = 0.5 *  height * self.a + 0.5 * height * self.b
+#         area = 0.5 *  height * self.a + 0.5 * height * self.b
 
-        return area  
+#         return area  
 
-first = Trapezia(4, 6)
-print(first.area(10))
+# first = Trapezia(4, 6)
+# print(first.area(10))
+class Family:
+    def __init__(self,Name,age):
+        self.Name= Name
+        self.age= age
+
+    def remarks(self):
+            return f'{self.Name}You are a grown up now'
+Child=Family('Name','age')
+Child.Name=input('Enter your name:')
+Child.age=input('Enter your age')
+
+print (f'name is:{Child.Name}')
+print(f'age is:{Child.age}')
+
+
+
