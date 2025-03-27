a762b217d00d260ed9533d384d1f7681fea86401
obonyodorice <obonyodorice3@gmail.com>
2025-03-27 13:33:52 +0300

    Fum

--- dorris.py
+++ dorris.py
@@ -23,18 +23,28 @@
 # first = Trapezia(4, 6)
 # print(first.area(10))
 class Family:
-    def __init__(self,Name,age):
+    def __init__(self,Name,age,con):
         self.Name= Name
         self.age= age
+        self.con= con
 
     def remarks(self):
-            return f'{self.Name}You are a grown up now'
-Child=Family('Name','age')
+            return f'{self.Name},You are a grown up now'
+
+Child=Family('Name','age','con')
 Child.Name=input('Enter your name:')
 Child.age=input('Enter your age')
+Child.con=input('Is the child old,(y/n)')
 
-print (f'name is:{Child.Name}')
-print(f'age is:{Child.age}')
+# print (f'name is:{Child.Name}')
+# print(f'age is:{Child.age}')
+while True:
+    if Child.con == 'n':
+        break
+    elif Child.con== 'y':
+        print (f'name is:{Child.Name}')
+        print(f'age is:{Child.age}')
 
-print(Child.remarks())
+        print(Child.remarks())
+        break
 
--- list.py
+++ list.py
@@ -1,6 +1,9 @@
 fruits=['Banana','Orange','Apple','Mango','Kiwi']
 # newlist=[x.upper() for x in fruits if x != 'Orange']
-newlist=[]
-newlist.append(fruits)
+# newlist=[]
 
-print(newlist)            
+# newlist.append(fruits)
+
+# print(newlist)           
+for i in range(len(fruits)):
+    print(f'{i}.{fruits[i]}')
