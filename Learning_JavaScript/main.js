2956e607df4d8cbbb83a02d7d0420ad28daeb00b
obonyodorice <obonyodorice3@gmail.com>
2025-04-08 13:09:28 +0300

    JS

---
+++ Learning_JavaScript/main.js
@@ -0,0 +1,9 @@
+let student =['Dorice','Bright','Chebor'];
+//console.log(student);
+const pupil={
+    Brighton:'Stupid',
+    Dorice:'Smart',
+    Chebor:'Cool'
+};
+console.log(pupil['Brighton']='Cute')
+console.log(pupil)
