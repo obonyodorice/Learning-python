37829ff6e057065c18cde2d5264e71a381999637
obonyodorice <obonyodorice3@gmail.com>
2025-05-29 04:28:24 -0700

    nm

--- dictionary.py
+++ dictionary.py
@@ -1,23 +1,23 @@
 # Sibling_colours={}
-# def Sibling_intro(Dictionary):
+def Sibling_intro(Dictionary):
 
-#     for key,val in Dictionary.items():
-#         print(f'I am {key} and {val} is my favourite colour')
-#     #    Sibling_colours={}
-# while True:
-#     Sibling_name =input('My name is:')
-#     Sibling_colour=input('My favourite colour is is:')
-#     Sibling_colours[Sibling_name]=Sibling_colour   
+    for key,val in Dictionary.items():
+        print(f'I am {key} and {val} is my favourite colour')
+Sibling_colours={}
+while True:
+    Sibling_name =input('My name is:')
+    Sibling_colour=input('My favourite colour is:')
+    Sibling_colours[Sibling_name]=Sibling_colour   
 
-#     another=input('add another(y/n)')
-#     if another=='y':   
-#         continue
-#     else:
-#         print("\nHere are my siblings names and there colours")
+    another=input('add another(y/n)')
+    if another=='y':   
+        continue
+    else:
+        # print("\nHere are my siblings names and there colours")
 
-#         Sibling_intro(Sibling_colours) 
-#         break
-
+        # Sibling_intro(Sibling_colours) 
+        break
+Sibling_intro(Sibling_colours) 
 
 
 
