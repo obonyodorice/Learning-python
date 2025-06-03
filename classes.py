abd165b4599612f123a85b96d62081c21c70e9a1
obonyodorice <obonyodorice3@gmail.com>
2025-06-03 06:30:43 -0700

    cm

--- classes.py
+++ classes.py
@@ -1,39 +1,3 @@
-# class Parent:
-#     hair_colour = 'Black'
-
-# class Child(Parent):
-#     pass
-
-# dorice = Child()
-# mom_dorice = Parent()
-
-# print(f'The hair colour of mom_dorice is:{mom_dorice.hair_colour} and that of dorice is:{dorice.hair_colour}')
-# class student:
-#     best_colour='Pink'
-# class teacher(student):
-#     pass
-# Dorice= teacher()
-# Child_Dorice=student()
-# print(f'{Dorice.best_colour} is my favourite colour and that of my child is also {Child_Dorice.best_colour}')
-# class Car:
-#     def __init__(self,Brand, Horsepower):
-#         self.Brand=Brand
-#         self.Horsepower=Horsepower
-#     def __str__(self):
-#         return f'{self.Brand} with {self.Horsepower}hp'
-
-#     # def __add__(self,other):
-#     #     return f'{self.Brand} & {other.Brand}'
-#     def Drive(self):
-#         return f'{self.Brand} is driving'
-#     # def speed(self):
-#     #     print(f'{self.Brand} with {self.Horsepower} Horsepower')
-# Toyota=Car('Toyota',200)
-# # Volvo=Car('Volvo',240)
-# print(Toyota)
-# # print(Toyota + Volvo)
-# Toyota.Drive()
-# # Toyota.speed()
 class Student:
     def __init__(self, name, age, course):
         self.name = name
@@ -46,12 +10,23 @@ class Student:
     def is_adult(self):
         return self.age >= 18
 
-# Example usage
-student1 = Student("Dorice", 22, "Telecommunications and IT")
+
+name = input("What's your name? ")
+age = int(input("..and your age? ")) 
+course = input("What course do you pursue? ")
+
+# Create a Student instance
+student1 = Student(name, age, course)
+
+
+print(f'\nName is: {student1.name}')
+print(f'Age is: {student1.age}')
+print(f'Course is: {student1.course}\n')
+
+
 student1.introduce()
 
 if student1.is_adult():
     print("This student is an adult.")
 else:
     print("This student is not an adult.")
-
