209d0c7c95e3370181b134b61126b3bb7d32e3c4
obonyodorice <obonyodorice3@gmail.com>
2025-06-27 11:03:45 -0700

    cm

--- area.py
+++ area.py
@@ -3,5 +3,12 @@
 # area=3.142*int(radius)**2
 # volume=area*length
 # print('The area is:',area,'and volume is:',volume)
-import random
-print(random.randrange(0,10))
+# import random
+# print(random.randrange(0,10))
+
+radius=input('Enter the radius: ')
+height=input('Enter the height: ')
+base_area=3.142*int(radius)**2
+Total_area=2*base_area + 2*3.142*int(radius)*int(height)
+Total_volume=base_area*int(height)
+print ('The area of a cylinder is :', Total_area, 'and Volume is:', Total_volume)
